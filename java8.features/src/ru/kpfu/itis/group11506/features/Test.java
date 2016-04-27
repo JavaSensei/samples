c2ac7eccad555de48754a980f7a4fc294dfeec54
zer0256 <forcheckitis@gmail.com>
2016-04-27 09:28:54 +0300

    Java 8 features

---
+++ java8.features/src/ru/kpfu/itis/group11506/features/Test.java
@@ -0,0 +1,7 @@
+package ru.kpfu.itis.group11506.features;
+
+/**
+ * Created by zer0 on 27.04.16.
+ */
+public class Test {
+}
---
+++ test1/src/ru/kpfu/itis/group11506/test1/Test.java
@@ -0,0 +1,7 @@
+package ru.kpfu.itis.group11506.test1;
+
+/**
+ * Created by zer0 on 13.04.16.
+ */
+public class Test {
+}
