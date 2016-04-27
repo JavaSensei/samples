2cd20da81f79d3646ac5d37fee5632a4fd297b4f
zer0256 <forcheckitis@gmail.com>
2016-04-27 09:43:59 +0300

    Added Java8 features

--- java8.features/src/ru/kpfu/itis/group11506/features/Main.java
+++ java8.features/src/ru/kpfu/itis/group11506/features/Main.java
@@ -1,7 +1,65 @@
 package ru.kpfu.itis.group11506.features;
 
-/**
- * Created by zer0 on 27.04.16.
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+
+    public static void main(String[] args) {
+        Test<Integer> test = new Test<Integer>() {
+
+            @Override
+            public boolean test(Integer testedParam) {
+                return testedParam > 0;
+            }
+
+        };
+
+        List<Student> studentList = new ArrayList<>();
+
+        final int minAge = 18;
+        final int diff = 5;
+
+
+        for (int index = 0; index<10; index++) {
+            int age = minAge + (int)(Math.random() * diff);
+            studentList.add(new Student("Name " + index, age, index));
+        }
+
+
+        System.out.println(studentList);
+        List<Student> byAge = filterByCondition(studentList,
+                student -> student.getAge() > 20
+        );
+
+
+        studentList.stream()
+                .filter(student -> student.getAge() > 20)
+                .filter(student -> student.getGroup() > 5)
+                .forEach(student -> System.out.println(student));
+
+
+
+        System.out.println(byAge);
+        filterByCondition(studentList, new Test<Student>() {
+            @Override
+            public boolean test(Student testedParam) {
+                return testedParam.getGroup() > 5;
+            }
+        });
+    }
+
+    private static List<Student> filterByCondition(
+            List<Student> sourceList,
+            Test<Student> test) {
+        List<Student> destList = new ArrayList<>();
+        for (Student student : sourceList) {
+            if (test.test(student)) {
+                destList.add(student);
+            }
+        }
+        return destList;
+    }
+
+
 }
--- java8.features/src/ru/kpfu/itis/group11506/features/Test.java
+++ java8.features/src/ru/kpfu/itis/group11506/features/Test.java
@@ -1,7 +1,8 @@
 package ru.kpfu.itis.group11506.features;
 
-/**
- * Created by zer0 on 27.04.16.
- */
-public class Test {
+@FunctionalInterface
+public interface Test<T> {
+
+    boolean test(T testedParam);
+
 }
